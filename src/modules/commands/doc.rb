c0742f1e5266de376e6aedee87f875a3b0e66749
Unknown <berkeucal@gmail.com>
2017-09-17 21:40:55 +0300

    Doc güncellendi

--- src/modules/commands/doc.rb
+++ src/modules/commands/doc.rb
@@ -2,7 +2,7 @@ module Bot::DiscordCommands
   module Doc
     extend Discordrb::Commands::CommandContainer
     command :doc do |event|
-        event.send '*' + event.message.author.name + '*' + ' *overhealed themselves at the beggining of the round*'
+        event.send '***' + event.message.author.name + '***' + ' *overhealed themselves at the beggining of the round*'
         puts event.message.author.name + " overhealed themselves"
     end
   end
